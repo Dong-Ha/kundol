3aa33216b981c7d6d71521a5c34ca8f6aa93a3bc
unknown <tlsehdgk33@gmail.com>
2023-08-21 21:04:14 +0900

    23-08-21

---
+++ 4_F_1062.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+
+int main(){
+    cin >> n >> k;
+    if(k < 5){
+        cout << "0" << '\n';
+        return 0;
+    }
+    
+    for(int i = 0; i < n; ++i){
+        string s;
+        cin >> s;
+        for(int j = 0; j < s.length(); ++j){
+
+        }
+    }
+
+
+    return 0;
+}
